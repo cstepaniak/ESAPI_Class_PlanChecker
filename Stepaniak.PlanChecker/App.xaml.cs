c5f37141f1792e5745882c464cd22a811d79330f
Christopher Stepaniak <cstepaniak@outlook.com>
2020-05-20 09:40:52 -0700

    Add project files.

---
+++ Stepaniak.PlanChecker/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ESAPIX.Bootstrapper;
+using Stepaniak.PlanChecker.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ESAPIX.Common;
+
+namespace Stepaniak.PlanChecker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App: Application
+    {
+        protected override void OnStartup(StartupEventArgs e) {
+            string[] args = e.Args;
+            base.OnStartup(e);
+            var bs = new AppBootstrapper<MainView>(() => { return VMS.TPS.Common.Model.API.Application.CreateApplication(); });
+            //You can use the following to load a context (for debugging purposes)
+            //args = ContextIO.ReadArgsFromFile(@"context.txt");
+            //Might disable (uncomment) for plugin mode
+            //bs.IsPatientSelectionEnabled = false;
+            bs.Run(args);
+        }
+
+        protected override void OnExit(ExitEventArgs e) {
+            AppComThread.Instance.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Stepaniak.PlanChecker/ViewModels/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using ESAPIX.Common;
+using Prism.Mvvm;
+using System.Linq;
+using VMS.TPS.Common.Model.API;
+
+namespace Stepaniak.PlanChecker.ViewModels
+{
+    public class MainViewModel: BindableBase
+    {
+        AppComThread VMS = AppComThread.Instance;
+
+        public MainViewModel() {
+            //Initialize with first loaded plan if any
+            OnPlanChanged(VMS.GetValue(sac => sac.PlanSetup));
+            //Handle plan changes (in standalone mode)
+            VMS.Execute(sac => sac.PlanSetupChanged += OnPlanChanged);
+        }
+
+        public void OnPlanChanged(PlanSetup ps) {
+            //Must operate on VMS thread for plan access
+            VMS.Invoke(() => {
+                Id = ps?.Id;
+                UID = ps?.UID;
+                IsDoseCalculated = ps?.Dose != null;
+                NBeams = ps?.Beams.Count();
+            });
+        }
+
+        private string id;
+
+        public string Id {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private string uid;
+
+        public string UID {
+            get { return uid; }
+            set { SetProperty(ref uid, value); }
+        }
+
+        private int? nBeams;
+
+        public int? NBeams {
+            get { return nBeams; }
+            set { SetProperty(ref nBeams, value); }
+        }
+
+        private bool isDoseCalculated;
+
+        public bool IsDoseCalculated {
+            get { return isDoseCalculated; }
+            set { SetProperty(ref isDoseCalculated, value); }
+        }
+    }
+}
